faa45555fc384289634543b58946e79966f2e1d8
davidpicariello <davidpicariello@davids-mbp>
2023-11-30 19:59:47 -0500

    begin reading api

---
+++ src/main/java/APIHelper.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author davidpicariello
+ */
+public class APIHelper {
+    String key = "key 8a1ca704a9cebf339819b785df15b9e7";
+    
+   public String getAPIDescription(String url){
+       // code from: https://docs.oracle.com/javase/tutorial/networking/urls/readingWriting.html
+        URL oracle;
+        try {
+            oracle = new URL(url);
+            URLConnection yc = oracle.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                                        yc.getInputStream()));
+            String inputLine;
+            StringBuilder sb = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+//                jTextArea1.append(inputLine);
+                sb.append(inputLine);
+            }
+            in.close();
+
+            return sb.toString();
+
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(AddOrderJframe.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(AddOrderJframe.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        // end code from above URL  
+        return null;
+    }
+       
+   }
+
+    
+}
