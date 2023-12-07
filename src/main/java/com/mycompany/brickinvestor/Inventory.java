148c5abfcfa1f2615ad95b93a59f941e78afa9ff
davidpicariello <davidpicariello@davids-mbp>
2023-12-07 11:21:52 -0500

    update addorder

--- src/main/java/com/mycompany/brickinvestor/Inventory.java
+++ src/main/java/com/mycompany/brickinvestor/Inventory.java
@@ -20,9 +20,26 @@ public class Inventory {
           
     }
     // Method to add an order to the inventory
-    public void addOrder(Order order) {
-        orders.add(order);
-    }
+    public void addOrder(Order newOrder) {
+        boolean orderExists = false;
+
+        for (Order existingOrder : orders) {
+            if (existingOrder.getDescription().equals(newOrder.getDescription())) {
+                // Combine fields if the orders match
+                existingOrder.setQuantity(existingOrder.getQuantity() + newOrder.getQuantity());
+                existingOrder.setCostPerItem(newOrder.getInitialCost(), newOrder.getRewardPoints());
+                orderExists = true;
+                break; // Exit the loop since the order has been processed
+            }
+        }
+
+        // If the order doesn't exist in the list, add it
+        if (!orderExists) {
+            orders.add(newOrder);
+        }
+
+        }
+    
 
     // Method to get the list of orders in the inventory
     public ArrayList<Order> getOrders() {
