c4c88da264cb9e6d1914f3868e3fa97d7e7d789b
davidpicariello <davidpicariello@davids-mbp>
2023-12-12 14:57:27 -0500

    before modify to add inventory array list

--- src/main/java/com/mycompany/brickinvestor/Inventory.java
+++ src/main/java/com/mycompany/brickinvestor/Inventory.java
@@ -5,6 +5,7 @@
 package com.mycompany.brickinvestor;
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -12,12 +13,22 @@ import java.util.ArrayList;
  */
 public class Inventory {
     //fields
+    private static Inventory instance;
     private ArrayList<Order> orders;
+    private ArrayList<Sale> sales;
   
     
-    public Inventory(){
+    private Inventory(){
         orders = new ArrayList<>();
+        sales = new ArrayList<>();
           
+    }
+    //Get method to return the only instance of Inventory
+    public static Inventory getInstance() {
+        if (instance == null) {
+            instance = new Inventory();
+        }
+        return instance;
     }
     // Method to add an order to the inventory
     public void addOrder(Order newOrder) {
@@ -27,9 +38,9 @@ public class Inventory {
             if (existingOrder.getDescription().equals(newOrder.getDescription())) {
                 // Combine fields if the orders match
                 existingOrder.setQuantity(existingOrder.getQuantity() + newOrder.getQuantity());
-                existingOrder.setCostPerItem(newOrder.getInitialCost(), newOrder.getRewardPoints());
+                existingOrder.setTotalCost(existingOrder.getTotalCost() + newOrder.getTotalCost());
                 orderExists = true;
-                break; // Exit the loop since the order has been processed
+                break; // Exit the loop order has been added
             }
         }
 
@@ -39,11 +50,62 @@ public class Inventory {
         }
 
         }
-    
+     //Method to add sale to inventory
+    public void addSale(Sale newSale) {
+        try {
+            for (Order existingOrder : orders) {
+                // Check if the order matches the setID and has enough quantity
 
+                if (existingOrder.getSetID() == (newSale.getSetID()) && (existingOrder.getQuantity() >= newSale.getQuantity())) {
+                    // Adjust the quantity of the existing order
+                    existingOrder.setQuantity(existingOrder.getQuantity() - newSale.getQuantity());
+                    // Adjust the totalCost of the existing order
+                    existingOrder.setTotalCost(existingOrder.getTotalCost()-(newSale.getQuantity()*newSale.getInitialCostPerItem()));
+
+                //quantity becomes zero then remove order
+                if (existingOrder.getQuantity() == 0) {
+                    orders.remove(existingOrder); // Remove the order from the list
+                }
+                // Exit the loop since the sale has been processed
+                break;
+                }
+            }
+            // Add the sale to the sales list
+            sales.add(newSale);
+             } catch (Exception e) {
+                // Handle the exception by displaying a panel
+                JOptionPane.showMessageDialog(null, "Error: Set ID not found or quantity too low", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+            }
+     // Method to remove an order based on the selected row index
+//    public void removeOrderBySelectedRow(Object setID) {
+//        //https://www.w3docs.com/snippets/java/how-to-cast-an-object-to-an-int.html
+//       int tmp = ((Integer) setID).intValue();
+//       for (Order existingOrder : orders) {
+//           if (existingOrder.getSetID() == tmp)
+//               
+//       }
+//       
+//    }
+    //Method to find existing order 
+    public Order findOrder(Object setID){
+          //https://www.w3docs.com/snippets/java/how-to-cast-an-object-to-an-int.html
+       int tmp = ((Integer) setID);
+       for (Order existingOrder : orders) {
+           if (existingOrder.getSetID() == tmp)
+               return existingOrder;
+               }
+       JOptionPane.showMessageDialog(null, "Error: Order not found", "Error", JOptionPane.ERROR_MESSAGE);
+        return null;
+    }
+    
     // Method to get the list of orders in the inventory
     public ArrayList<Order> getOrders() {
         return orders;
+    }
+    // Method to get the list of sales in the inventory
+    public ArrayList<Sale> getSales() {
+        return sales;
     }
     
 }
