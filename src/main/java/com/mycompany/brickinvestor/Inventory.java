a37a6ec3aad6414a1e4c55e633cca84945083498
davidpicariello <davidpicariello@davids-mbp>
2023-12-15 09:13:33 -0500

    final save

--- src/main/java/com/mycompany/brickinvestor/Inventory.java
+++ src/main/java/com/mycompany/brickinvestor/Inventory.java
@@ -16,78 +16,94 @@ public class Inventory {
     private static Inventory instance;
     private ArrayList<Order> orders;
     private ArrayList<Sale> sales;
-  
+    private ArrayList<Order> inventory;
+    private float totalAmountInvested;
+    private int totalQuantity;
     
     private Inventory(){
         orders = new ArrayList<>();
         sales = new ArrayList<>();
+        inventory = new ArrayList<>();
           
     }
     //Get method to return the only instance of Inventory
+    //https://www.w3schools.blog/java-singleton-design-pattern
     public static Inventory getInstance() {
         if (instance == null) {
             instance = new Inventory();
         }
         return instance;
     }
-    // Method to add an order to the inventory
-    public void addOrder(Order newOrder) {
-        boolean orderExists = false;
+    //Method to add order to orders
+    public void addOrder(Order newOrder){
+        orders.add(newOrder);
 
-        for (Order existingOrder : orders) {
-            if (existingOrder.getDescription().equals(newOrder.getDescription())) {
-                // Combine fields if the orders match
-                existingOrder.setQuantity(existingOrder.getQuantity() + newOrder.getQuantity());
-                existingOrder.setTotalCost(existingOrder.getTotalCost() + newOrder.getTotalCost());
-                orderExists = true;
-                break; // Exit the loop order has been added
+    }
+    //Method to add sale to sales
+    public void addSale(Sale newSale){
+        sales.add(newSale);
+    }
+    
+    // Method to add an order to the inventory
+    public void addInventory(Order newOrder) {
+        boolean orderExists = false;
+        if (inventory.size() >= 1) {
+            for (Order existingOrder : inventory) {
+                if (existingOrder.getDescription().equals(newOrder.getDescription())) {
+                    // Combine fields if the orders match
+                    existingOrder.setQuantity(existingOrder.getQuantity() + newOrder.getQuantity());
+                    existingOrder.setTotalCost(existingOrder.getTotalCost() + newOrder.getTotalCost());
+                    orderExists = true;
+                    break; // Exit the loop order has been added
+                }
             }
         }
-
-        // If the order doesn't exist in the list, add it
+        // If the order doesn't exist or inventory is empty add order
         if (!orderExists) {
-            orders.add(newOrder);
+            inventory.add(new Order(newOrder.getSetID()
+                    ,newOrder.getInitialCost()
+                    ,newOrder.getQuantity()
+                    ,newOrder.getRewardPoints()
+                    ,newOrder.getDescription()
+                    ,newOrder.getSupplier()
+                    ,newOrder.getNumPieces()
+                    ,newOrder.getUrl()
+                    ,newOrder.getDatePurchased()
+                    ,newOrder.getYearReleased()));
         }
-
+            totalAmountInvested += newOrder.getTotalCost();
+            totalQuantity += newOrder.getQuantity();
+            
         }
+  
+    
      //Method to add sale to inventory
-    public void addSale(Sale newSale) {
+    public void removeInventory(Sale newSale) {
         try {
-            for (Order existingOrder : orders) {
+            for (Order existingOrder : inventory) {
                 // Check if the order matches the setID and has enough quantity
-
                 if (existingOrder.getSetID() == (newSale.getSetID()) && (existingOrder.getQuantity() >= newSale.getQuantity())) {
                     // Adjust the quantity of the existing order
                     existingOrder.setQuantity(existingOrder.getQuantity() - newSale.getQuantity());
                     // Adjust the totalCost of the existing order
                     existingOrder.setTotalCost(existingOrder.getTotalCost()-(newSale.getQuantity()*newSale.getInitialCostPerItem()));
-
-                //quantity becomes zero then remove order
+                //quantity becomes zero than remove order
                 if (existingOrder.getQuantity() == 0) {
-                    orders.remove(existingOrder); // Remove the order from the list
+                    inventory.remove(existingOrder); // Remove the order from the inventory list
                 }
                 // Exit the loop since the sale has been processed
                 break;
                 }
             }
-            // Add the sale to the sales list
-            sales.add(newSale);
+            
              } catch (Exception e) {
                 // Handle the exception by displaying a panel
-                JOptionPane.showMessageDialog(null, "Error: Set ID not found or quantity too low", "Error", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Error: Set ID not found", "Error", JOptionPane.ERROR_MESSAGE);
             }
+            totalAmountInvested -= (newSale.getInitialCostPerItem()*newSale.getQuantity());
+            totalQuantity -= newSale.getQuantity();
             }
-     // Method to remove an order based on the selected row index
-//    public void removeOrderBySelectedRow(Object setID) {
-//        //https://www.w3docs.com/snippets/java/how-to-cast-an-object-to-an-int.html
-//       int tmp = ((Integer) setID).intValue();
-//       for (Order existingOrder : orders) {
-//           if (existingOrder.getSetID() == tmp)
-//               
-//       }
-//       
-//    }
-    //Method to find existing order 
+    //Method to find existing order searches orders becasue if sold all quality it won't be in inventory
     public Order findOrder(Object setID){
           //https://www.w3docs.com/snippets/java/how-to-cast-an-object-to-an-int.html
        int tmp = ((Integer) setID);
@@ -98,14 +114,61 @@ public class Inventory {
        JOptionPane.showMessageDialog(null, "Error: Order not found", "Error", JOptionPane.ERROR_MESSAGE);
         return null;
     }
-    
-    // Method to get the list of orders in the inventory
+   
+    public void updateInventoryList(){
+        inventory.clear();
+        totalAmountInvested = 0;
+        totalQuantity = 0;
+        //pass orders into inventory
+        for (Order existingOrder : orders) {
+                addInventory(existingOrder);
+            }
+        //remove each sale from inventory
+            for (Sale existingSale : sales) {
+                 removeInventory(existingSale);
+            }
+        //update the Inventory Jtable
+        CurrentInventoryJFrame.setInventoryJTable();
+    }
+    // Method to get the list of orders in the orderlog
     public ArrayList<Order> getOrders() {
         return orders;
     }
-    // Method to get the list of sales in the inventory
+    // Method to get the list of sales in the salelog
     public ArrayList<Sale> getSales() {
         return sales;
     }
+    // Method to get the list of orders in the inventory
+    public ArrayList<Order> getInventory() {
+        return inventory;
+    }
     
+    //Method to get total amount invested
+    public float getTotalAmountInvested(){
+        return totalAmountInvested;
+    }
+    //method to get total quanity of inventory
+    public int getTotalQuantity(){
+        return totalQuantity;
+    }
+ 
+    //Setter Method sales
+    public void setSales(ArrayList<Sale> sales){
+        this.sales = sales;
+    }
+    //Setter Method orders
+    public void setOrders(ArrayList<Order> orders){
+        this.orders = orders;
+    }
+    //Setter Method inventory
+    public void setInventory(ArrayList<Order> inventory){
+        this.inventory = inventory;
+    }
+    
+    public void setTotalAmountInvested(float totalAmountInvested){
+        this.totalAmountInvested = totalAmountInvested;
+    }
+    public void setTotalQuantity(int totalQuantity){
+        this.totalQuantity = totalQuantity;
+    }
 }
